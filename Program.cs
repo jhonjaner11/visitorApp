c6d3345330c78103fb28b0ef307896384a30c57c
Janer <jhonjaner11@gmail.com>
2020-02-07 19:17:30 -0500

    git ignore

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace visitorApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
